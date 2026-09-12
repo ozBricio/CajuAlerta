75246ec20801627c335462d6efcc3b52914043f8
ozBricio <contato.woftgames@gmail.com>
2026-09-12 19:56:19 -0300

    Fix: Atualiza frase do enigma do banho para maior coerência lógica

--- builder.py
+++ builder.py
@@ -1,11 +1,14 @@
 ﻿import io
 import re
 
-with io.open('frontend/cadastro.html', 'r', encoding='utf-8', errors='ignore') as f:
-    html = f.read()
+with io.open('frontend/assets/js/caju-security.js', 'r', encoding='utf-8', errors='ignore') as f:
+    cjs = f.read()
 
-html = re.sub(r'<div class="terms-group">(\s*<legend>)', r'<fieldset class="terms-group">\1', html)
-html = re.sub(r'(</label>\s*)</div>(\s*<button type="submit" id="btnSubmitCadastro")', r'\1</fieldset>\2', html)
+cjs = cjs.replace('"Onde voc\\u011B toma banho"', '"Objetos usados no banho ou encontrados no banheiro"')
+cjs = cjs.replace('"Onde você toma banho"', '"Objetos usados no banho ou encontrados no banheiro"')
+cjs = cjs.replace('"Onde vocǦ toma banho"', '"Objetos usados no banho ou encontrados no banheiro"')
+cjs = cjs.replace('Onde você toma banho', 'Objetos usados no banho ou encontrados no banheiro')
+cjs = cjs.replace('Onde voc\u011b toma banho', 'Objetos usados no banho ou encontrados no banheiro')
 
-with io.open('frontend/cadastro.html', 'w', encoding='utf-8') as f:
-    f.write(html)
+with io.open('frontend/assets/js/caju-security.js', 'w', encoding='utf-8') as f:
+    f.write(cjs)
