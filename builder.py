a70d598ffe11227e31860ae0a71ee0c6c9534d6f
ozBricio <contato.woftgames@gmail.com>
2026-09-12 20:12:20 -0300

    Fix: Padroniza o UI/UX de Registrar e Perfil com o Header e Footer principais e corrige responsividade

--- builder.py
+++ builder.py
@@ -1,11 +1,14 @@
 ﻿import io
 import re
 
-for filename in ['frontend/index.html', 'frontend/login.html', 'frontend/cadastro.html', 'frontend/registrar.html', 'frontend/perfil.html']:
+for filename in ['frontend/registrar.html', 'frontend/perfil.html']:
     with io.open(filename, 'r', encoding='utf-8', errors='ignore') as f:
         html = f.read()
     
-    html = re.sub(r'assets/js/common\.js(\?v=\d+)?', 'assets/js/common.js?v=3', html)
+    html = re.sub(r'assets/js/common\.js(\?v=\d+)?', 'assets/js/common.js?v=4', html)
+    html = re.sub(r'assets/css/common\.css(\?v=\d+)?', 'assets/css/common.css?v=4', html)
+    html = re.sub(r'assets/css/registrar\.css(\?v=\d+)?', 'assets/css/registrar.css?v=4', html)
+    html = re.sub(r'assets/css/perfil\.css(\?v=\d+)?', 'assets/css/perfil.css?v=4', html)
     
     with io.open(filename, 'w', encoding='utf-8') as f:
         f.write(html)
